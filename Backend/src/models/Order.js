0842b328ff67e85d622aef0352725c2566439694
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2025-08-12 10:50:01 +0530

    Product and order model created

---
+++ Backend/src/models/Order.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    farmer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: [1, "Quantity must be at least 1"],
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: [
+        "pending",
+        "confirmed",
+        "out for delivery",
+        "delivered",
+        "cancelled",
+      ],
+      default: "pending",
+    },
+    deliveryAdress: {
+      type: String,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["wallet", "razorpay", "cod"],
+      required: true,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);
---
+++ Backend/src/models/Product.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required : [true, "Name is required"],
+        trim: true
+    },
+    price:{
+        type: Number,
+        required : [true, "Price is required"],
+        min: [0, "Price must be positive number"]
+    },
+    quantity:{
+        type: Number,
+        required : [true, "Quantity is required"],
+        min: [0, "Quantity must be at least 0"]
+        
+    },
+    category:{
+        type: String,
+        required : [true, "Category is required"],
+        enum: ["fruits", "vegetables", "dairy", "grains", "other"]
+    },
+    harvestDate:{
+        type: Date,
+        required: [true, "Harvest date is required"]
+    },
+    image:{
+        type: String,
+        required : [true, "Image is required"]
+    },
+    farmer:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required : true
+    },
+}, { timestamps: true })
+
+export default mongoose.model("Product", productSchema)
